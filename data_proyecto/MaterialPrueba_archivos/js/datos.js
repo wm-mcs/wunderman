34323fa7f04468c23fde46a6ffb2aadb6a67f166
Mauricio Costanzo <ventas@worldmaster.com.uy>
2021-02-10 19:26:19 -0300

    slider

---
+++ data_proyecto/MaterialPrueba_archivos/js/datos.js
@@ -0,0 +1,6 @@
+var dptosLocs = {
+	"Artigas":["Artigas","Bella Unión"],
+	"Canelones":["Canelones","Santa Lucía"],
+	"Montevideo":["Montevideo"],
+	"Salto":["Salto","Daymán","Arapey"]
+};
--- webpack.config.js
+++ webpack.config.js
@@ -3,30 +3,62 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-	mode: "development",
-	context: __dirname,
-	entry: {
-		home: path.resolve(__dirname, "src", "assets", "js", "index.js")
-	},
+  mode: "development",
+  context: __dirname,
+  entry: {
+    home: path.resolve(__dirname, "src", "assets", "js", "index.js")
+  },
 
-	output: {
-		path: path.resolve(__dirname, "docs"),
-		filename: "js/[name].js"
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: [{ loader: MiniCssExtractPlugin.loader }, "css-loader"]
-			}
-		]
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: path.resolve(__dirname, "src/index.html")
-		}),
-		new MiniCssExtractPlugin({
-			filename: "css/[name].css"
-		})
-	]
+  output: {
+    path: path.resolve(__dirname, "docs"),
+    filename: "js/[name].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/
+      },
+
+      {
+        test: /\.css$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.scss$/,
+
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+
+      {
+        test: /\.jpg|png|gif$/,
+        use: { loader: "url-loader", options: { limit: 9000 } }
+      },
+      {
+        test: /\.(ttf|eot|svg|gif|woff|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: [
+          {
+            loader: "url-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/index.html")
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css"
+    })
+  ]
 };
