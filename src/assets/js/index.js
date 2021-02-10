0d20e22dc9523dad8fd7100d6498965a7ccf0ed0
Mauricio <ventas@worldmaster.com.uy>
2021-02-10 07:34:51 -0300

    base

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,1 +1,5 @@
+import "./../css/index.css";
+
+document.body.innerHTML = '<p class="parrafo"> Hola ¿Cómo estás rer?</p>';
+
 console.log("hola");
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,32 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
-  context: __dirname,
-  entry: {
-    home: path.resolve(__dirname, "src", "assets", "js", "index.js")
-  },
+	mode: "development",
+	context: __dirname,
+	entry: {
+		home: path.resolve(__dirname, "src", "assets", "js", "index.js")
+	},
 
-  output: {
-    path: path.resolve(__dirname, "docs"),
-    filename: "js/[name].js"
-  }
+	output: {
+		path: path.resolve(__dirname, "docs"),
+		filename: "js/[name].js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [{ loader: MiniCssExtractPlugin.loader }, "css-loader"]
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, "src/index.html")
+		}),
+		new MiniCssExtractPlugin({
+			filename: "css/[name].css"
+		})
+	]
 };
